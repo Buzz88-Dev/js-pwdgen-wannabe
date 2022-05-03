a896a24f409d6e98c55d178dea13240939945e4a
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-03 15:31:33 +0200

    aggiornamento

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const nome = prompt("Come ti chiami?");
+console.log(nome);
+
+const cognome = prompt("Qual'è il tuo cognome?");
+console.log(cognome);
+
+const colore = prompt("Qual'è il tuo colore preferito?");
+console.log(colore);
+
+document.getElementById("mio_id").innerHTML = 'Ciao, mi chiamo ' + nome + " " + cognome + " e il mio colore preferito è il " + colore;
+
+document.getElementById("secondo_id").innerHTML = `Mi chiamo ${nome} ${cognome} e il mio colore preferito è il ${colore}`;
